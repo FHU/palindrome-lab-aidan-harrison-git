6e51faa1cdf221f335acf3e10309a37e18d078ea
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-02-06 12:22:36 -0600

    main.py
    passed all tests and ready to go

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    if word.isspace() == True:
+        return False
+    comparison = word.lower()
+    list = comparison.split()
+    xwhite = ''.join(list)
+    # print(xwhite)
+    upper_bound = len(xwhite) - 1
+    xwhite_reverse = []
+    
+    for index in range(upper_bound,-1, -1):
+        xwhite_reverse.append(xwhite[index])
+    # print(xwhite_reverse)
+    xwhite_reverse_str = ''.join(xwhite_reverse)
+    # print(xwhite_reverse_str)
+    if xwhite_reverse_str == xwhite:
+        return True
+    else:
+        return False
+
+    
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    word = input()
+    print(palindrome(word))
